c557ea49ac45732af9638fd89686cb5a0e78d5d0
jtoram <jtoram+github@aleeas.com>
2022-10-04 03:25:23 +0200

    Initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,57 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useContract, useContractEvents } from "@thirdweb-dev/react";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import { BigNumber, ethers } from "ethers";
+import { useState } from "react";
 import "./styles/Home.css";
+import usdcAbi from "./UsdcAbi.json";
 
 export default function Home() {
+
+  const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const sdk = new ThirdwebSDK("mainnet");
+  const usdcContract = sdk.getContractFromAbi(usdcAddress, usdcAbi);
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [value, setValue] = useState("");
+  let blockNumber = 0;
+  let transactionIndex = 0;
+
+  usdcContract.events.listenToAllEvents(
+    (event) => {
+      if (event.transaction.blockNumber > blockNumber && event.transaction.transactionIndex > transactionIndex) {
+        blockNumber = event.transaction.blockNumber;
+        transactionIndex = event.transaction.transactionIndex;
+        if (event.eventName === "Transfer" && BigNumber.from(event.data.value._hex).gt(BigNumber.from(1000000000))) {
+          console.log(event);
+          setFrom(event.data.from);
+          setTo(event.data.to);
+          setValue(ethers.utils.formatUnits(BigNumber.from(event.data.value._hex), 6));
+        }
+      }
+    }
+  );
+
   return (
     <div className="container">
       <main className="main">
         <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+          Welcome to the Ethereum Blockchain Watcher
         </h1>
 
         <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/main.tsx</code>, then modify the{" "}
-          <code className="code">src/App.tsx</code> file!
+          Watch the USDC transfers on the Ethereum Blockchain!
         </p>
 
-        <div className="connect">
-          <ConnectWallet />
-        </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
+        <div>
+            <p className="code">
+              From: {from}
             </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
+            <p className="code">
+              To: {to}
             </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
+            <p className="code">
+              Value: {value}
             </p>
-          </a>
         </div>
       </main>
     </div>
